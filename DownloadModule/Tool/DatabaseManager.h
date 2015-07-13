129eb8f18731ac84d700d3240d4d8fec5e0ccad2
ytdxxt10 <ytdxxt10@163.com>
2015-07-13 17:11:44 +0800

    modify

---
+++ DownloadModule/Tool/DatabaseManager.h
@@ -0,0 +1,13 @@
+//
+//  DatabaseManager.h
+//  DownloadModule
+//
+//  Created by offcn on 15/7/13.
+//  Copyright (c) 2015年 Terry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DatabaseManager : NSObject
+
+@end
---
+++ DownloadModule/Tool/DatabaseManager.m
@@ -0,0 +1,13 @@
+//
+//  DatabaseManager.m
+//  DownloadModule
+//
+//  Created by offcn on 15/7/13.
+//  Copyright (c) 2015年 Terry. All rights reserved.
+//
+
+#import "DatabaseManager.h"
+
+@implementation DatabaseManager
+
+@end
